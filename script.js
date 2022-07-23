2807c1f9591b65dd40c0cd702d83c43f8f65a72b
Sneha Hegde <snehahegde33@gmail.com>
2022-07-23 15:40:31 +0530

    modal

--- script.js
+++ script.js
@@ -20,7 +20,13 @@ const closeModal = () => {
 
 for(let i = 0;i< btnsOpenModal.length;i++){
     btnsOpenModal[i].addEventListener('click', openModal);
-    btnCloseModal.addEventListener('click', closeModal);
-    overlay.addEventListener('click', closeModal);
-
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' &&!modal.classList.contains('hidden')){
+        closeModal();
+    }
+});
